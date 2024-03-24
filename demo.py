17a925b4d1eade807e76d631da6c7cef63688ba3
aadityaadyot <aadityaadyot@gmail.com>
2024-03-24 19:00:19 +0530

    Demo done for data ingestion

--- demo.py
+++ demo.py
@@ -4,4 +4,4 @@ from hate.exception import CustomException
 from hate.configuration.gcloud_syncer import GCloudSync
 
 obj = GCloudSync()
-obj.sync_folder_from_gcloud("hate-speech2024", "dataset.zip", "download/dataset.zip")
+obj.sync_folder_from_gcloud("hate_speech_data2024", "dataset.zip", "dataset.zip")
